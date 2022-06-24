c63f24884688511e90db2cc0033a3930d701b04a
dmalac <dmalacov@student.codam.nl>
2022-06-24 17:59:51 +0200

    tester basis one column improved

--- tester_push_swap/test_script_basis_one_column.sh
+++ tester_push_swap/test_script_basis_one_column.sh
@@ -4,9 +4,12 @@
 min=5
 max=7
 TRANCHES=$min
+NAME_PATH=../
 NAME=push_swap
+TESTER_DIR=tester_push_swap
 
 # checks whether the necessary executables are present and compiles the missing ones
+cd ..
 while [ $TRANCHES -le $max ]
 do
 	if [ ! -f "$NAME"_$TRANCHES ]
@@ -16,34 +19,35 @@ do
 	fi
 	TRANCHES=$((TRANCHES + 1))
 done
+cd $TESTER_DIR
 
 # testing
 
-PREVIOUS_ID=1005
+PREVIOUS_ID=3006
 ID=1
-REPEAT=1995
+REPEAT=10
 while [ $ID -le $REPEAT ]
 do
 	INPUT=$(sort -R input.txt)
 	TRANCHES=$min
-	CHECKER=$(MallocNanoZone=0 ./"$NAME"_$TRANCHES $INPUT | ./checker_Mac $INPUT)
+	#CHECKER=$(MallocNanoZone=0 ./"$NAME_PATH""$NAME"_$TRANCHES $INPUT | ../checker_Mac $INPUT)
+	CHECKER=$(./"$NAME_PATH""$NAME"_$TRANCHES $INPUT | ../checker_Mac $INPUT)
 	while [ $TRANCHES -le $max ]
 	do
 		echo $(($ID + $PREVIOUS_ID)) >> result.txt
 		echo $INPUT >> result.txt
 		echo $CHECKER >> result.txt
 		echo $TRANCHES >> result.txt
-		echo $(MallocNanoZone=0 ./"$NAME"_$TRANCHES $INPUT | wc -l) >> result.txt
+		#echo $(MallocNanoZone=0 ./"$NAME_PATH""$NAME"_$TRANCHES $INPUT | wc -l) >> result.txt
+		echo $(./"$NAME_PATH""$NAME"_$TRANCHES $INPUT | wc -l) >> result.txt
 		echo "*" >> result.txt
 		TRANCHES=$((TRANCHES + 1))
 	done
 	ID=$((ID + 1))
 done
 
-#cp result.txt result_backup.txt
-
 tr '\n' ';' < result.txt > temp.txt
 sed -i '' 's/;\*;/\*/g' temp.txt
 tr '\*' '\n' < temp.txt > result.txt
 cat result.txt >> result_total.txt
-rm temp.txt #result.txt
+rm temp.txt result.txt
